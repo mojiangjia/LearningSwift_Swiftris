37baf0d8b6b769891cb7d9f89bc558a70c67d37b
mojiangjia <mojjia@seas.upenn.edu>
2017-05-27 10:10:13 +0800

    modify GameScene to add drawing logic

--- Swiftris/GameScene.swift
+++ Swiftris/GameScene.swift
@@ -10,16 +10,26 @@ import SpriteKit
 import GameplayKit
 
 // s or ms?
-let TickLengthLevelOne = TimeInterval(0.6)
+let TickLengthLevelOne = TimeInterval(600)
+
+// Point size of each block sprite, in our case 20.0 x 20.0
+let BlockSize: CGFloat = 20.0
 
 class GameScene: SKScene {
+    
+    let gameLayer = SKNode()
+    let shapeLayer = SKNode()
+    //  Layer position which will give us an offset from the edge of the screen
+    let LayerPosition = CGPoint(x: 6, y: -6)
     
     var tick : (() -> ())?
     var tickLengthMillis = TickLengthLevelOne
     var lastTick : NSDate?
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var textureCache = Dictionary<String, SKTexture>()
+    
+//    private var label : SKLabelNode?
+//    private var spinnyNode : SKShapeNode?
     
 //    override func didMove(to view: SKView) {
 //        
@@ -50,36 +60,48 @@ class GameScene: SKScene {
     
     override init(size: CGSize) {
         super.init(size: size)
+        
         anchorPoint = CGPoint(x: 0, y: 1.0)
         let background = SKSpriteNode(imageNamed: "background")
         background.position = CGPoint(x: 0, y: 0)
         background.anchorPoint = CGPoint(x: 0, y: 1.0)
+        
         addChild(background)
+        addChild(gameLayer)
+        
+        let gameBoardTexture = SKTexture(imageNamed: "gameboard")
+        let gameBoard = SKSpriteNode(texture: gameBoardTexture, size: CGSize(width: BlockSize * CGFloat(NumColumns), height: BlockSize * CGFloat(NumRows)))
+        gameBoard.anchorPoint = CGPoint(x:0, y:1.0)
+        gameBoard.position = LayerPosition
+
+        shapeLayer.position = LayerPosition
+        shapeLayer.addChild(gameBoard)
+        gameLayer.addChild(shapeLayer)
     }
     
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
+//    func touchDown(atPoint pos : CGPoint) {
+//        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
+//            n.position = pos
+//            n.strokeColor = SKColor.green
+//            self.addChild(n)
+//        }
+//    }
+//    
+//    func touchMoved(toPoint pos : CGPoint) {
+//        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
+//            n.position = pos
+//            n.strokeColor = SKColor.blue
+//            self.addChild(n)
+//        }
+//    }
+//    
+//    func touchUp(atPoint pos : CGPoint) {
+//        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
+//            n.position = pos
+//            n.strokeColor = SKColor.red
+//            self.addChild(n)
+//        }
+//    }
     
 //    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
 //        if let label = self.label {
@@ -89,18 +111,18 @@ class GameScene: SKScene {
 //        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
 //    }
     
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
+//    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+//    }
+//    
+//    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+//    }
+//    
+//    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+//    }
+//    
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
@@ -125,5 +147,52 @@ class GameScene: SKScene {
     
     func stopTicking() {
         lastTick = nil
+    }
+    
+    func pointForColumn(column: Int, row: Int) -> CGPoint {
+        let x = LayerPosition.x + (CGFloat(column) * BlockSize) + (BlockSize / 2)
+        let y = LayerPosition.y - ((CGFloat(row) * BlockSize) + (BlockSize / 2))
+        return CGPoint(x: x, y: y)
+    }
+    
+    func addPreviewShapeToScene(shape: Shape, completion: @escaping () -> ()) {
+        for block in shape.blocks {
+            var texture = textureCache[block.spriteName]
+            if texture == nil {
+                texture = SKTexture(imageNamed: block.spriteName)
+                textureCache[block.spriteName] = texture
+            }
+            let sprite = SKSpriteNode(texture: texture)
+            // start it at row - 2, such that the preview piece animates smoothly into place from a higher location.
+            sprite.position = pointForColumn(column: block.column, row: block.row - 2)
+            shapeLayer.addChild(sprite)
+            block.sprite = sprite
+            
+            // Animation
+            sprite.alpha = 0
+            
+            let moveToAction = SKAction.move(to: pointForColumn(column: block.column, row: block.row), duration: TimeInterval(0.2))
+            moveToAction.timingMode = .easeOut
+            let fadeInAction = SKAction.fadeAlpha(to: 0.7, duration: 0.4)
+            fadeInAction.timingMode = .easeOut
+            sprite.run(SKAction.group([moveToAction, fadeInAction]))
+        
+        }
+        run(SKAction.wait(forDuration: 0.4), completion: completion)
+    }
+    
+    
+    func redrawShape(shape:Shape, completion:@escaping () -> ()) {
+        for block in shape.blocks {
+            let sprite = block.sprite!
+            let moveTo = pointForColumn(column: block.column, row:block.row)
+            let moveToAction = SKAction.move(to: moveTo, duration: 0.05)
+            moveToAction.timingMode = .easeOut
+            if block == shape.blocks.last {
+                sprite.run(moveToAction, completion: completion)
+            } else {
+                sprite.run(moveToAction)
+            }
+        }
     }
 }
